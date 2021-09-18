ca08e942d4a7488566781beb6349d1bf18c98998
Sree Sai Raghava <raghava.rym@gmail.com>
2021-09-18 14:42:35 +0530

    Initial Commit

---
+++ IIQLogin/IIQLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  IIQLoginApp.swift
+//  IIQLogin
+//
+//  Created by Sree Sai Raghava Dandu on 18/09/21.
+//
+
+import SwiftUI
+
+@main
+struct IIQLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
